6ac5eec474d6d642f299adc85af9fb3bc2a658f6
Artem Popov <artem.petrovich001@bk.ru>
2024-07-03 09:12:11 +0300

    fix_task2

--- Task_2/multiprocessing_solution_2.py
+++ Task_2/multiprocessing_solution_2.py
@@ -4,24 +4,36 @@ import multiprocessing
 
 from prime_factors import prime_factors_count
 
-# Функция подсчета с использованием multiprocessing
-def worker(numbers):
+def worker(numbers, result_queue, lock):
     total_count = 0
     for number in numbers:
         total_count += prime_factors_count(number)
-    return total_count
+    with lock:
+        result_queue.put(total_count)
 
 def multiprocessing_prime_factors_count(file_path):
     with open(file_path, 'r') as f:
         numbers = [int(line.strip()) for line in f]
 
-    cpu_count = os.cpu_count()
+    cpu_count = multiprocessing.cpu_count()
     chunk_size = len(numbers) // cpu_count
     chunks = [numbers[i:i + chunk_size] for i in range(0, len(numbers), chunk_size)]
 
-    with multiprocessing.Pool(cpu_count) as pool:
-        results = pool.map(worker, chunks)
-    return sum(results)
+    result_queue = multiprocessing.Queue()
+    lock = multiprocessing.Lock()
+    processes = []
+    for chunk in chunks:
+        p = multiprocessing.Process(target=worker, args=(chunk, result_queue, lock))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    total_count = 0
+    while not result_queue.empty():
+        total_count += result_queue.get()
+    return total_count
 
 
 if __name__ == "__main__":
